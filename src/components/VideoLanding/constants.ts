693c9d956d782357ba1a6716a75aa1f71f1b91f7
realkyr <phuree.kanusont@gmail.com>
2025-05-10 02:52:56 +0700

    Update VIDEO_URL with a new token in constants.ts
    Replaced the old token in the VIDEO_URL constant with a new one to ensure continued access to the video resource. This change prevents potential issues caused by an expired or invalid token.

--- src/components/VideoLanding/constants.ts
+++ src/components/VideoLanding/constants.ts
@@ -1,2 +1,2 @@
 export const VIDEO_URL =
-  'https://firebasestorage.googleapis.com/v0/b/likq-38cdb.firebasestorage.app/o/landing-opt.mp4?alt=media&token=ae526909-7c00-4798-907b-67c04664b726'
+  'https://firebasestorage.googleapis.com/v0/b/likq-38cdb.firebasestorage.app/o/landing-opt.mp4?alt=media&token=dee1e936-da39-46ea-9c74-dbbb2fb76314'
