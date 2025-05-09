d9b418b93845122c8fc0523df11d2d8bab130836
realkyr <phuree.kanusont@gmail.com>
2025-05-10 01:17:01 +0700

    Add Noto Sans Thai font integration
    Integrated Noto Sans Thai font for supporting Thai script alongside Latin. Updated the global CSS to use Noto Sans Thai as the default font for better locale support.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
-import { inter } from '@/utils/font'
+import { inter, notoSans } from '@/utils/font'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} ${notoSans.variable} antialiased`}
       >
         {children}
       </body>
