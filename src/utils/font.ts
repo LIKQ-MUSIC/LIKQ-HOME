9db0f3fc3f8935a19c8d86ac532de64057b955e0
realkyr <phuree.kanusont@gmail.com>
2025-05-10 01:17:01 +0700

    Add Noto Sans Thai font integration
    Integrated Noto Sans Thai font for supporting Thai script alongside Latin. Updated the global CSS to use Noto Sans Thai as the default font for better locale support.

--- src/utils/font.ts
+++ src/utils/font.ts
@@ -1,7 +1,14 @@
-import { Inter } from 'next/font/google'
+import { Inter, Noto_Sans_Thai } from 'next/font/google'
 
 export const inter = Inter({
   weight: 'variable',
   subsets: ['latin'],
   variable: '--font-inter'
+})
+
+export const notoSans = Noto_Sans_Thai({
+  subsets: ['thai', 'latin'],
+  weight: ['400', '700'], // or ["100", "300", "400", "500", "700", "900"]
+  display: 'swap',
+  variable: '--font-noto-sans-thai'
 })
