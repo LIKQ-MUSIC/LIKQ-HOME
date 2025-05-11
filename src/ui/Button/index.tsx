0b187d8d647abab37f3b9119d2d95f66150ddb18
realkyr <phuree.kanusont@gmail.com>
2025-05-12 00:20:32 +0700

    Refactor buttons to use forwardRef for improved flexibility
    Updated the Button and Carousel components to integrate React's forwardRef, enabling better ref handling and improving compatibility with parent components. Added a displayName for the Button component for enhanced debugging and development visibility.

--- src/ui/Button/index.tsx
+++ src/ui/Button/index.tsx
@@ -1,39 +1,47 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import { cn } from '@/utils'
 
 import ClientRipple from './Ripple'
 import { ButtonProps } from './types'
 import { buttonSizesClass, variantClasses } from './constants'
 
-const Button = ({
-  children,
-  variant = 'primary',
-  size = 'lg',
-  className,
-  onClick,
-  disabled
-}: ButtonProps) => {
-  const defaultClasses = variantClasses[variant]
-  const defaultSize = buttonSizesClass[size]
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      children,
+      variant = 'primary',
+      size = 'lg',
+      className,
+      onClick,
+      disabled
+    },
+    ref
+  ) => {
+    const defaultClasses = variantClasses[variant]
+    const defaultSize = buttonSizesClass[size]
 
-  return (
-    <button
-      onClick={onClick}
-      disabled={disabled}
-      className={cn([
-        `relative overflow-hidden font-medium
-        transition-all duration-300 ease-in-out hover:shadow-lg active:scale-95`,
-        defaultSize,
-        defaultClasses,
-        className,
-        disabled &&
-          'bg-disabled text-disabled-text cursor-not-allowed hover:bg-disabled'
-      ])}
-    >
-      {children}
-      <ClientRipple />
-    </button>
-  )
-}
+    return (
+      <button
+        ref={ref}
+        onClick={onClick}
+        disabled={disabled}
+        className={cn([
+          `relative overflow-hidden font-medium
+          transition-all duration-300 ease-in-out hover:shadow-lg active:scale-95`,
+          defaultSize,
+          defaultClasses,
+          className,
+          disabled &&
+            'bg-disabled text-disabled-text cursor-not-allowed hover:bg-disabled'
+        ])}
+      >
+        {children}
+        <ClientRipple />
+      </button>
+    )
+  }
+)
+
+Button.displayName = 'Button'
 
 export default Button
