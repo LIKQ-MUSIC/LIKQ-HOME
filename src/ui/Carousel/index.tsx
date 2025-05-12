72947b30b1110a8d8f15ed23b0f1784021739eae
realkyr <phuree.kanusont@gmail.com>
2025-05-12 13:04:18 +0700

    Add new icons, adjust styling, and update carousel behavior
    New icons such as Arrange, Fansong, and MixMaster were added. Carousel button styles and positioning were refined for consistency. Minor CSS and SVG adjustments ensure better alignment and responsive design.

--- src/ui/Carousel/index.tsx
+++ src/ui/Carousel/index.tsx
@@ -205,9 +205,9 @@ const CarouselPrevious = React.forwardRef<
       variant={variant}
       // size={size}
       className={cn(
-        'absolute  h-8 w-8 rounded-full border-none p-0 flex justify-center items-center',
+        'absolute h-8 w-8 rounded-full border-none p-0 flex justify-center items-center !bg-transparent',
         orientation === 'horizontal'
-          ? '-left-12 top-1/2 -translate-y-1/2'
+          ? '-left-8 lg:-left-12 top-1/2 -translate-y-1/2'
           : '-top-12 left-1/2 -translate-x-1/2 rotate-90',
         className
       )}
@@ -233,9 +233,9 @@ const CarouselNext = React.forwardRef<
       ref={ref}
       variant={variant}
       className={cn(
-        'absolute h-8 w-8 rounded-full border-none p-0 flex justify-center items-center',
+        'absolute h-8 w-8 rounded-full border-none p-0 flex justify-center items-center !bg-transparent',
         orientation === 'horizontal'
-          ? '-right-12 top-1/2 -translate-y-1/2'
+          ? '-right-8 md:-right-12 top-1/2 -translate-y-1/2'
           : '-bottom-12 left-1/2 -translate-x-1/2 rotate-90',
         className
       )}
